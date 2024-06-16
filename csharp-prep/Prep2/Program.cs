31a14c956edeb5bab6dfae9ef1fca24cd6fee15f
SammyAwe <awesamuelopeoluwa@gmail.com>
2024-06-15 21:53:54 -0700

    assignment commits

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,59 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        Console.Write("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+
+        
+        int gradePercentage;
+        bool isValid = int.TryParse(input, out gradePercentage);
+
+        
+        if (!isValid || gradePercentage < 0 || gradePercentage > 100)
+        {
+            Console.WriteLine("Please enter a valid grade percentage between 0 and 100.");
+        }
+        else
+        {
+            
+            string letter;
+
+            if (gradePercentage >= 90)
+            {
+                letter = "A";
+            }
+            else if (gradePercentage >= 80)
+            {
+                letter = "B";
+            }
+            else if (gradePercentage >= 70)
+            {
+                letter = "C";
+            }
+            else if (gradePercentage >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+
+            
+            Console.WriteLine($"Your letter grade is {letter}.");
+
+            
+            if (gradePercentage >= 70)
+            {
+                Console.WriteLine("Congratulations! You passed the course.");
+            }
+            else
+            {
+                Console.WriteLine("Unfortunately, you did not pass the course. Better luck next time!");
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        
+        do
+        {
+            Console.Write("Enter number: ");
+            if (int.TryParse(Console.ReadLine(), out number))
+            {
+                if (number != 0)
+                {
+                    numbers.Add(number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter an integer.");
+            }
+        } while (number != 0);
+
+        if (numbers.Count > 0)
+        {
+            
+            int sum = numbers.Sum();
+            Console.WriteLine($"The sum is: {sum}");
+
+            
+            double average = numbers.Average();
+            Console.WriteLine($"The average is: {average}");
+
+            
+            int max = numbers.Max();
+            Console.WriteLine($"The largest number is: {max}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
     }
 }
